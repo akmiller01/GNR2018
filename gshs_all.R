487524f1c8c4eb4a3bf581045fa2cb4809414109
Alex Miller <alex.k.miller@gmail.com>
2018-05-03 16:23:01 -0400

    PDF scraping!

--- gshs_all.R
+++ gshs_all.R
@@ -1,15 +1,16 @@
-list.of.packages <- c("foreign","readr","scrapeR","data.table","stringr", "tools")
+list.of.packages <- c("foreign","readr","scrapeR","data.table","stringr", "tools","devtools","rJava","varhandle")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 lapply(list.of.packages, require, character.only=T)
 
+# install_github("ropenscilabs/tabulizerjars")
+# install_github("ropenscilabs/tabulizer")
+library(tabulizer)
+
 wd = "~/GNR/gshs_data"
 unlink("~/GNR/gshs_data/*",recursive=T, force=T)
 setwd(wd)
-
-meta_vars = read_csv("../gshs_vars_meta.csv")
-soda_files = subset(meta_vars,description=="soda")$filename
 
 base_link = "http://www.who.int"
 datasets_page_source = scrape("http://www.who.int/ncds/surveillance/gshs/datasets/en/",headers=T,follow=T,parse=T)[[1]]
@@ -18,7 +19,6 @@ link_suffixes = sapply(link_elems,xmlGetAttr,"href")
 link_suffixes = unique(c(link_suffixes,"/ncds/surveillance/gshs/myanmardataset/en/","/ncds/surveillance/gshs/bahamasdataset/en/"))
 
 data.list = list()
-var.lab.list = list()
 data.index = 1
 
 full_links = paste0(base_link,link_suffixes)
@@ -27,6 +27,8 @@ for(full_link in full_links){
   country_page_source = scrape(full_link,headers=T,follow=T,parse=T)[[1]]
   media_link_elems = getNodeSet(country_page_source,"//a[@class='link_media']")
   media_links = sapply(media_link_elems,xmlGetAttr,"href")
+  pdf_links = subset(media_links,grepl("pdf",media_links,ignore.case=T))
+  nat_pdf = paste0(base_link,pdf_links[1])
   media_links = subset(media_links,grepl("spss",media_links,ignore.case=T))
   national_link = paste0(base_link,media_links[1])
   year = str_extract(national_link,"([1-2][0-9][0-9][0-9])")
@@ -35,21 +37,44 @@ for(full_link in full_links){
   download.file(national_link,tmp)
   
   unzipped = unzip(tmp)
+  
+  tabs = extract_tables(nat_pdf, guess=F, area=list(c(168,44,792,216)))
+  # tabs = extract_tables(nat_pdf, guess=F, columns = list(c(54,102,162)), area=list(c(162,54,792,220)))
+  tabs.list = list()
+  tabs.index = 1
+  for(tab in tabs){
+    if(ncol(tab)==4){
+      tabs.list[[tabs.index]] = data.frame(tab)[c("X1","X2","X3")]
+      tabs.index = tabs.index + 1
+    }else if(ncol(tab)==3){
+      tabs.list[[tabs.index]] = data.frame(tab)
+      tabs.index = tabs.index + 1
+    }else if(ncol(tab)==2){
+      temp.tab = data.frame(tab)
+      temp.tab$X3 = temp.tab$X2
+      tabs.list[[tabs.index]] = temp.tab
+      tabs.index = tabs.index + 1
+    }
+  }
+  standard.names = rbindlist(tabs.list)
+  standard.names[which(standard.names$X3=="" | is.na(standard.names$X3))]$X3 = standard.names[which(standard.names$X3=="" | is.na(standard.names$X3))]$X2
+  standard.names = standard.names[,c("X2","X3"),with=FALSE]
+  names(standard.names) = c("standard","non.standard")
+  standard.names$standard = unfactor(standard.names$standard)
+  standard.names$non.standard = unfactor(standard.names$non.standard)
   if(tolower(file_ext(unzipped))=="sav"){
     #Unzipped is a file
     file_base = basename(unzipped)
     gshs_nat = read.spss(unzipped, use.value.labels = F)
-    var_labs = data.frame(names(gshs_nat),attributes(gshs_nat)$variable.labels)
-    names(var_labs) = c("variable.name","variable.label")
-    var_labs$country = country_name
-    var_labs$year = year
-    var_labs$filename = file_base
     gshs_nat = data.frame(gshs_nat)
+    names(gshs_nat) = toupper(names(gshs_nat))
+    standard.names = subset(standard.names, non.standard %in% names(gshs_nat))
+    gshs_nat = gshs_nat[standard.names$non.standard]
+    names(gshs_nat) = standard.names$standard
     gshs_nat$country = country_name
     gshs_nat$year = year
     gshs_nat$filename = file_base
     data.list[[data.index]] = gshs_nat
-    var.lab.list[[data.index]] = var_labs
     data.index = data.index + 1 
   }else{
     #Unzipped is a directory
@@ -58,26 +83,21 @@ for(full_link in full_links){
     for(sav in savs){
       file_base = basename(sav)
       gshs_nat = read.spss(sav, use.value.labels = F)
-      var_labs = data.frame(names(gshs_nat),attributes(gshs_nat)$variable.labels)
-      names(var_labs) = c("variable.name","variable.label")
-      var_labs$country = country_name
-      var_labs$year = year
-      var_labs$filename = file_base
       gshs_nat = data.frame(gshs_nat)
+      names(gshs_nat) = toupper(names(gshs_nat))
+      gshs_nat = gshs_nat[standard.names$non.standard]
+      names(gshs_nat) = standard.names$standard
       gshs_nat$country = country_name
       gshs_nat$year = year
       gshs_nat$filename = file_base
       data.list[[data.index]] = gshs_nat
-      var.lab.list[[data.index]] = var_labs
       data.index = data.index + 1 
     }
   }
 }
 
 gshs = rbindlist(data.list,fill=T)
-gshs_vars = rbindlist(var.lab.list)
 
 save(gshs,gshs_vars,file="../gshs_all.RData")
-write_csv(gshs_vars,"../gshs_vars_all.csv")
 all_file_names = unique(data.frame(gshs)[c("filename","country","year")])
 write_csv(all_file_names,"../all_file_names.csv")
