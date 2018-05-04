44e0a8fceadd7f1f7a6ed360adece5a63c7c803f
Alex Miller <alex.k.miller@gmail.com>
2018-05-04 13:06:31 -0400

    analysis

--- gshs_all.R
+++ gshs_all.R
@@ -1,12 +1,8 @@
-list.of.packages <- c("foreign","readr","scrapeR","data.table","stringr", "tools","devtools","rJava","varhandle")
+list.of.packages <- c("foreign","readr","scrapeR","data.table","stringr", "tools")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 lapply(list.of.packages, require, character.only=T)
-
-# install_github("ropenscilabs/tabulizerjars")
-# install_github("ropenscilabs/tabulizer")
-library(tabulizer)
 
 wd = "~/GNR/gshs_data"
 unlink("~/GNR/gshs_data/*",recursive=T, force=T)
@@ -18,9 +14,8 @@ link_elems = getNodeSet(datasets_page_source,"//ul[@class='list_dash']/li/a")
 link_suffixes = sapply(link_elems,xmlGetAttr,"href")
 link_suffixes = unique(c(link_suffixes,"/ncds/surveillance/gshs/myanmardataset/en/","/ncds/surveillance/gshs/bahamasdataset/en/"))
 
-skip_standardization = c("benin")
-
 data.list = list()
+var.lab.list = list()
 data.index = 1
 
 full_links = paste0(base_link,link_suffixes)
@@ -29,8 +24,6 @@ for(full_link in full_links){
   country_page_source = scrape(full_link,headers=T,follow=T,parse=T)[[1]]
   media_link_elems = getNodeSet(country_page_source,"//a[@class='link_media']")
   media_links = sapply(media_link_elems,xmlGetAttr,"href")
-  pdf_links = subset(media_links,grepl("pdf",media_links,ignore.case=T))
-  nat_pdf = paste0(base_link,pdf_links[1])
   media_links = subset(media_links,grepl("spss",media_links,ignore.case=T))
   national_link = paste0(base_link,media_links[1])
   year = str_extract(national_link,"([1-2][0-9][0-9][0-9])")
@@ -39,57 +32,22 @@ for(full_link in full_links){
   download.file(national_link,tmp)
   
   unzipped = unzip(tmp)
-  if(!country_name %in% skip_standardization){
-    tabs = extract_tables(nat_pdf, guess=F, area=list(c(168,44,792,220)))
-    # tabs = extract_tables(nat_pdf, guess=F, columns = list(c(54,102,162)), area=list(c(162,54,792,220)))
-    tabs.list = list()
-    tabs.index = 1
-    for(tab in tabs){
-      if(ncol(tab)==4){
-        tabs.list[[tabs.index]] = data.frame(tab)[c("X1","X2","X3")]
-        tabs.index = tabs.index + 1
-      }else if(ncol(tab)==3){
-        tabs.list[[tabs.index]] = data.frame(tab)
-        tabs.index = tabs.index + 1
-      }else if(ncol(tab)==2){
-        temp.tab = data.frame(tab)
-        temp.tab$X3 = temp.tab$X2
-        tabs.list[[tabs.index]] = temp.tab
-        tabs.index = tabs.index + 1
-      }
-    }
-    standard.names = rbindlist(tabs.list)
-    standard.names[which(standard.names$X3=="" | is.na(standard.names$X3))]$X3 = standard.names[which(standard.names$X3=="" | is.na(standard.names$X3))]$X2
-    standard.names = standard.names[,c("X2","X3"),with=FALSE]
-    names(standard.names) = c("standard","non.standard")
-    standard.names$standard = unfactor(standard.names$standard)
-    standard.names$non.standard = unfactor(standard.names$non.standard) 
-  }else{
-    if(exists("standard.names")){
-      rm(standard.names)
-    }
-  }
   if(tolower(file_ext(unzipped))=="sav"){
     #Unzipped is a file
     file_base = basename(unzipped)
     gshs_nat = read.spss(unzipped, use.value.labels = F)
+    var_labs = data.frame(names(gshs_nat),attributes(gshs_nat)$variable.labels)
+    names(var_labs) = c("variable.name","variable.label")
+    var_labs$country = country_name
+    var_labs$year = year
+    var_labs$filename = file_base
     gshs_nat = data.frame(gshs_nat)
     names(gshs_nat) = toupper(names(gshs_nat))
-    if(!country_name %in% skip_standardization){
-      message("Number of standard names not in names: ",sum(!standard.names$standard %in% names(gshs_nat)))
-      message("Number of nonstandard names not in names: ",sum(!standard.names$non.standard %in% names(gshs_nat)))
-      if(sum(!standard.names$standard %in% names(gshs_nat))>0){
-        #Not already standardized
-        message("Standardizing!")
-        standard.names = subset(standard.names, non.standard %in% names(gshs_nat))
-        gshs_nat = gshs_nat[standard.names$non.standard]
-        names(gshs_nat) = standard.names$standard 
-      }
-    }
     gshs_nat$country = country_name
     gshs_nat$year = year
     gshs_nat$filename = file_base
     data.list[[data.index]] = gshs_nat
+    var.lab.list[[data.index]] = var_labs
     data.index = data.index + 1 
   }else{
     #Unzipped is a directory
@@ -97,31 +55,28 @@ for(full_link in full_links){
     savs = list.files(path=unzipped, pattern="*.sav", full.names= T, recursive = T, ignore.case= T)
     for(sav in savs){
       file_base = basename(sav)
-      gshs_nat = read.spss(unzipped, use.value.labels = F)
+      gshs_nat = read.spss(sav, use.value.labels = F)
+      var_labs = data.frame(names(gshs_nat),attributes(gshs_nat)$variable.labels)
+      names(var_labs) = c("variable.name","variable.label")
+      var_labs$country = country_name
+      var_labs$year = year
+      var_labs$filename = file_base
       gshs_nat = data.frame(gshs_nat)
       names(gshs_nat) = toupper(names(gshs_nat))
-      if(!country_name %in% skip_standardization){
-        message("Number of standard names not in names: ",sum(!standard.names$standard %in% names(gshs_nat)))
-        message("Number of nonstandard names not in names: ",sum(!standard.names$non.standard %in% names(gshs_nat)))
-        if(sum(!standard.names$standard %in% names(gshs_nat))>0){
-          #Not already standardized
-          message("Standardizing!")
-          standard.names = subset(standard.names, non.standard %in% names(gshs_nat))
-          gshs_nat = gshs_nat[standard.names$non.standard]
-          names(gshs_nat) = standard.names$standard 
-        }
-      }
       gshs_nat$country = country_name
       gshs_nat$year = year
       gshs_nat$filename = file_base
       data.list[[data.index]] = gshs_nat
+      var.lab.list[[data.index]] = var_labs
       data.index = data.index + 1 
     }
   }
 }
 
 gshs = rbindlist(data.list,fill=T)
+gshs_vars = rbindlist(var.lab.list)
 
 save(gshs,gshs_vars,file="../gshs_all.RData")
+write_csv(gshs_vars,"../gshs_vars_all.csv")
 all_file_names = unique(data.frame(gshs)[c("filename","country","year")])
 write_csv(all_file_names,"../all_file_names.csv")
---
+++ gshs_analysis.R
@@ -0,0 +1,64 @@
+list.of.packages <- c("data.table","readr","survey","Hmisc")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+lapply(list.of.packages, require, character.only=T)
+
+setwd("~/GNR")
+
+load("gshs_all.RData")
+
+
+meta_vars = read_csv("gshs_vars_meta.csv")
+soda_files = subset(meta_vars,description=="soda")$filename
+
+gshs$fruit = gshs$Q7
+gshs$no.fruit = as.integer(gshs$fruit==1)
+gshs$daily.fruit = as.integer(gshs$fruit>=3)
+gshs$veg = gshs$Q8
+gshs$no.veg = as.integer(gshs$veg==1)
+gshs$daily.veg = as.integer(gshs$veg>=3)
+gshs$soda = NA
+gshs$soda[which(gshs$filename %in% soda_files)] = gshs$Q9[which(gshs$filename %in% soda_files)]
+gshs$no.soda = as.integer(gshs$soda==1)
+gshs$daily.soda = as.integer(gshs$soda>=3)
+
+#Mongolia is missing survey weights
+gshs = subset(gshs, country!="mongolia")
+countries = unique(gshs$country)
+tab.list = list()
+for(ctry in countries){
+  message(ctry)
+  dat = subset(gshs,country==ctry)
+  year = unique(dat$year)
+  dsn = svydesign(ids = ~1, data = dat, weights = ~WEIGHT)
+  if(sum(!is.na(dat$fruit))>0){
+    no.fruit = svymean(~no.fruit, design = dsn,na.rm=T)[1]
+    daily.fruit = svymean(~daily.fruit, design = dsn,na.rm=T)[1]
+  }else{
+    no.fruit = NA
+    daily.fruit = NA
+  }
+  if(sum(!is.na(dat$veg))>0){
+    no.veg = svymean(~no.veg, design = dsn,na.rm=T)[1]
+    daily.veg = svymean(~daily.veg, design = dsn,na.rm=T)[1]
+  }else{
+    no.veg = NA
+    daily.veg = NA
+  }
+  if(sum(!is.na(dat$soda))>0){
+    no.soda = svymean(~no.soda, design = dsn,na.rm=T)[1]
+    daily.soda = svymean(~daily.soda, design = dsn,na.rm=T)[1]
+  }else{
+    no.soda = NA
+    daily.soda = NA
+  }
+  
+  df = data.frame(country=ctry,year,no.fruit,daily.fruit,no.veg,daily.veg,no.soda,daily.soda)
+  tab.list[[ctry]] = df
+}
+
+tab = rbindlist(tab.list, fill=T)
+write_csv(tab,"gshs_tab.csv")
+
+
