6fad075beb7460f26eea3e22b959fc447d290080
Alex Miller <alex.k.miller@gmail.com>
2018-06-06 08:36:12 -0400

    work

--- spot_check_prev.R
+++ spot_check_prev.R
@@ -47,3 +47,25 @@ sum(dat$weights*sapply(dat$h11_1,na.in,in_c=c(1,2)),na.rm=T) +
   sum(dat$weights*sapply(dat$h11_5,na.in,in_c=c(1,2)),na.rm=T) +
   sum(dat$weights*sapply(dat$h11_6,na.in,in_c=c(1,2)),na.rm=T)
 # 8687.131
+
+# Another way of counting N
+sum(dat$weights*!is.na(dat$h11_1),na.rm=T) +
+  sum(dat$weights*!is.na(dat$h11_2),na.rm=T) +
+  sum(dat$weights*!is.na(dat$h11_3),na.rm=T) +
+  sum(dat$weights*!is.na(dat$h11_4),na.rm=T) +
+  sum(dat$weights*!is.na(dat$h11_5),na.rm=T) +
+  sum(dat$weights*!is.na(dat$h11_6),na.rm=T)
+
+#Deaths for children born in the last 5 years?
+d_u_5 = 0
+for(i in 1:20){
+  if(i<10){
+    num = paste0("0",i)
+  }else{num=i}
+  dob = paste0("b3_",num)
+  aad = paste0("b7_",num)
+  d_u_5 = d_u_5 + sum(
+    (dat[aad]<=60)*dat$weights*((dat$v008-dat[dob])<=1825)
+    ,na.rm=T)
+}
+# 9701.538
